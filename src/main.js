2c13e6d9157bed267bae87da4ac22955085b6a3d
22rubychou <70134566+22rubychou@users.noreply.github.com>
2020-12-14 10:39:36 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueSweetalert2 from 'vue-sweetalert2'
@@ -12,22 +9,21 @@ import 'sweetalert2/dist/sweetalert2.min.css'
 import ImgInputer from 'vue-img-inputer'
 import 'vue-img-inputer/dist/index.css'
 import Photoswipe from 'vue-pswipe'
-import VueAnalytics from 'vue-analytics'
 
-Vue.use(Photoswipe)
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.component('ImgInputer', ImgInputer)
-Vue.config.productionTip = false
 axios.defaults.withCredentials = true
 
+Vue.config.productionTip = false
 
 Vue.use(VueAxios, axios)
 Vue.use(VueSweetalert2)
-Vue.use(VueAnalytics, {
-  id:process.env.VUE_APP_GA
-})
+Vue.use(Photoswipe)
+Vue.component('ImgInputer', ImgInputer)
 
-new Vue ({
+new Vue({
   router,
   store,
   render: h => h(App)
